363cdb6dd6a8c399ba08465cdeba29b4832d035e
Paulo Vidal <pvidalx@gmail.com>
2026-02-03 10:02:11 +0000

    Update lib/source.ts
    Co-authored-by: Ando “Thor” Nando  <divinegod@gmail.com>

--- lib/source.ts
+++ lib/source.ts
@@ -34,10 +34,3 @@ export function getPageImage(page: InferPageType<typeof source>) {
   };
 }
 
-export async function getLLMText(page: InferPageType<typeof source>) {
-  const processed = await page.data.getText('processed');
-
-  return `# ${page.data.title}
-
-${processed}`;
-}
